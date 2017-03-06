765253cd4673523c4c2cf388deecfedcd43270a4
Wesley3452 <wesleyvanrooyen8@gmail.com>
2017-03-06 13:25:05 +0200

    app

--- src/app/addmember/addmember.component.ts
+++ src/app/addmember/addmember.component.ts
@@ -13,10 +13,3 @@ export class AddmemberComponent implements OnInit {
   }
 
 }
-
-@Component({
-selector: 'add-site-view',
-templateUrl: 'app/add-site.template.html'
-})
-export class AddSiteComponent {
-}
